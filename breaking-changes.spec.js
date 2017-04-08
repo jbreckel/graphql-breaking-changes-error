95e00223e79b91dd836db22c97c0701d657af4b7
Julius Breckel <julius.breckel@signavio.com>
2017-04-08 12:18:31 +0300

    simplified test

--- breaking-changes.spec.js
+++ breaking-changes.spec.js
@@ -1,16 +1,29 @@
 const {
   findBreakingChanges,
   buildClientSchema,
+  graphql,
 } = require('graphql')
 
-const oldIntrospection = require('./schema.json')
-
-const newSchema = require('./schema')
-
-const oldSchema = buildClientSchema(oldIntrospection.data)
+const { introspectionQuery } = require('graphql/utilities')
+const { makeExecutableSchema } = require('graphql-tools')
 
 describe('Breaking Changes', () => {
-  it('there should be no breaking changes', () => {
-    expect(findBreakingChanges(oldSchema, newSchema)).toEqual([])
+  it('test with graphql-tools', async () => {
+    const schemaString = `
+    schema {
+      query: Query
+    }
+
+    type Query {
+      test(id: ID!): String
+    }
+    `
+
+    const Schema = makeExecutableSchema({
+      typeDefs: schemaString,
+    })
+
+    const { data, errors } = await graphql(Schema, introspectionQuery)
+    expect(findBreakingChanges(buildClientSchema(data), Schema)).toEqual([])
   })
 })
